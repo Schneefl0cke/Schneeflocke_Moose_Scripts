7bd06a6c2c7e39101a11876b3c149553be01b6c4
Florian <floheid@7fun.de>
2024-10-05 16:56:58 +0200

    Working example + comments

--- Omnien Campaign/CAP_intercept_example.lua
+++ Omnien Campaign/CAP_intercept_example.lua
@@ -1,3 +1,5 @@
+-- Example for the CAP_Intercept Script
+
 -- This script creates CAP and Interceptors in given zones using Moose. You will need to extend and adapt it based on the examples in the script. 
 -- The goal is randomization, since I play the missions I create myself but dont want to know everything that happens.
 -- you still need to adapt the SETUP_FIGHTER_COMMAND function to your liking, until I add more decent randomization
@@ -15,12 +17,13 @@ math.random()
 -- General variables
 local _faction_red = true --set to false if you want a blue CAP/Intercept
 local _name_of_faction = "Omnien"
-local _name_prefix_of_EWR_Units = "RED EWR" -- All units with this as their prefix in the name will be added as information for the fighter command. You must create some EWR units, since the CAP needs ground control to intercept. 
-local _homeland = ZONE_POLYGON:NewFromGroupName("Homeland") --Create a unit called 'Homeland', the waypoints of the unit will be the main area of defense
+local _name_prefix_of_EWR_Units = "RED_EWR" -- All units with this as their prefix in the name will be added as information for the fighter command. You must create some EWR units, since the CAP needs ground control to intercept. 
+local _homeland = ZONE_POLYGON:NewFromGroupName("Red_Border") --Create a unit called 'Homeland', the waypoints of the unit will be the main area of defense
 --
 
 -- CAP zone names
-local _cap_zone_1_name = "CAP ZONE 1"
+local _cap_zone_1_name = "Red_Bandar_AirZone"
+local _cap_zone_2_name = "Red_Kish_AirZone"
 
 -- Fighter Setup. If you want to add more airframe variation, you must extend the GetFighterType function!
 local _fighter_min = 4 -- the minnimum available fighters per squadron
@@ -29,23 +32,26 @@ local _mission_range_min = 100
 local _mission_range_max = 200
 
 --fighter types
-local _fighter_type_1 = "red_fighter_1" -- Name of the unit in DCS. Give the unit the correct loadout and skin!
+local _fighter_type_1 = "Omnien-AF-Skyhawk" -- Name of the unit in DCS. Give the unit the correct loadout and skin!
 local _readiness_fighter_1 = 85 -- Propability that the fighters are on station, or all are grounded. As percentage value of 100
 local _fighter_type_1_capability = 50 -- how good is the fighter
 
-local _fighter_type_2 = "red_fighter_2" -- Name of the unit in DCS. Give the unit the correct loadout and skin!
+local _fighter_type_2 = "Omnien-AF-F-5" -- Name of the unit in DCS. Give the unit the correct loadout and skin!
 local _readiness_fighter_2 = 70 -- Propability that the fighters are on station, or all are grounded. As percentage value of 100
 local _fighter_type_2_capability = 70
 --
 
 --warehouses
-local _warehouse_1 = "Warehouse_1" -- You need to add a structure next to an airport with this name.
+local _warehouse_1 = "Warehouse Bandar" -- You need to add a structure next to an airport with this name.
+local _warehouse_2 = "Warehouse Kish" -- You need to add a structure next to an airport with this name.
 --
 
 -- names
 local _airwing_1_name = "Airwing 1"
+local _airwing_2_name = "Airwing 2"
 local _squadron_1_name = " Squadron 1" -- space because it will be merged with faction name
 local _squadron_2_name = " Squadron 2"
+local _squadron_3_name = " Squadron 3"
 
 -- How is the grouping of the enemy flights? The sum does not need to be 100
 local _propability_1_ship_flight = 40
@@ -53,6 +59,10 @@ local _propability_2_ship_flight = 40
 local _propability_3_ship_flight = 0
 local _propability_4_ship_flight = 20
 --
+
+--------------------------------------
+--| CHANGE BELOW HERE AT OWN RISK! |--
+--------------------------------------
 
 -- Chief Setup
 local _agents=SET_GROUP:New():FilterPrefixes(_name_prefix_of_EWR_Units):FilterOnce()
@@ -62,6 +72,7 @@ if _faction_red == true then
 else
     _fighter_command = CHIEF:New(coalition.side.BLUE, _agents)
 end
+
 _fighter_command:AddBorderZone(_homeland)
 _fighter_command:SetTacticalOverviewOn()
 
@@ -82,7 +93,7 @@ local function GetFighterGroupingCount()
     end
 end
 
-local function setup_airwings()
+local function setup_airwing_1 ()
 
     --- AIRWING 1
     local _airwing_1=AIRWING:New(_warehouse_1, _airwing_1_name) --Ops.AirWing#AIRWING
@@ -103,7 +114,7 @@ local function setup_airwings()
 	    local missionrange = math.random(_mission_range_min, _mission_range_max)
 	    _squadron:SetMissionRange(missionrange) -- Squad will be considered for targets within 200 NM of its airwing location.
 	    _airwing_1:AddSquadron(_squadron)
-    end 
+    end
 
     -- SQUADRON 2
     if math.random(_readiness_fighter_2) < 100 then
@@ -120,16 +131,44 @@ local function setup_airwings()
     end
 end
 
+local function setup_airwing_2()
+     --- AIRWING 2
+     local _airwing_2 = AIRWING:New(_warehouse_2, _airwing_2_name) --Ops.AirWing#AIRWING
+     _airwing_2:NewPayload(_fighter_type_1, -1, {AUFTRAG.Type.GCICAP, AUFTRAG.Type.INTERCEPT, AUFTRAG.Type.CAP}, 80)
+     _airwing_2:NewPayload(_fighter_type_2, -1, {AUFTRAG.Type.GCICAP, AUFTRAG.Type.INTERCEPT, AUFTRAG.Type.CAP}, 80)
+     _fighter_command:AddAirwing(_airwing_2)
+     --******
+
+    -- SQUADRON 3
+    if math.random(_readiness_fighter_1) < 100 then
+        local _available_fighters = math.random(_fighter_min, _fighter_max)
+        local _squadron_3 = SQUADRON:New(_fighter_type_1, _available_fighters, _name_of_faction .. _squadron_3_name)
+        _squadron_3:SetGrouping(GetFighterGroupingCount()) -- Random grouping
+	    _squadron_3:SetModex(300)  -- Tail number of the sqaud start with 130, 131,...
+	    _squadron_3:AddMissionCapability({AUFTRAG.Type.INTERCEPT}, _fighter_type_1_capability) -- Squad can do intercept missions.
+        _squadron_3:AddMissionCapability({AUFTRAG.Type.ALERT5})        -- Squad can be spawned at the airfield in uncontrolled state.
+        _squadron_3:AddMissionCapability(AUFTRAG.Type.CAP,_fighter_type_1_capability)
+	    local missionrange = math.random(_mission_range_min, _mission_range_max)
+	    _squadron_3:SetMissionRange(missionrange) -- Squad will be considered for targets within 200 NM of its airwing location.
+	    _airwing_2:AddSquadron(_squadron_3)
+    end
+end
+
 local function setup_cap_zones()
     -- CAP only between 0500 and 20000
 	if (timer.getAbsTime() > 18000 and timer.getAbsTime() < 72000) then
 		--CAP Zones
 		local _CAP_zone_1 = ZONE_POLYGON:NewFromGroupName(_cap_zone_1_name)
 		_fighter_command:AddCapZone(_CAP_zone_1,15000,240,270,30) -- Zone, Altitude, Speed, Heading, Leg
-		_fighter_command:AddCapZone(_CAP_zone_1,15000,240,270,30) -- second group!
+		_fighter_command:AddCapZone(_CAP_zone_1,20000,240,270,30) -- second group!
+
+        local _CAP_zone_2 = ZONE_POLYGON:NewFromGroupName(_cap_zone_2_name)
+		_fighter_command:AddCapZone(_CAP_zone_2,15000,240,270,30) -- Zone, Altitude, Speed, Heading, Leg
+        _fighter_command:AddCapZone(_CAP_zone_2,20000,240,270,30) -- second group
 	end
 end
 
-setup_airwings()
+setup_airwing_1()
+setup_airwing_2()
 setup_cap_zones() -- CAP is only spawned at day!
 _fighter_command:__Start(1)
